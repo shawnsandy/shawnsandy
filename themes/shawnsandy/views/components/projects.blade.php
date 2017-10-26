c1b831f18c13e8f125d315c53260df6ffa82f415
Shawn Sandy <shawnsandy04@gmail.com>
2017-10-26 12:15:36 -0400

    update : add aos on project section

--- themes/shawnsandy/views/components/projects.blade.php
+++ themes/shawnsandy/views/components/projects.blade.php
@@ -1,13 +1,43 @@
- <div class="section is-medium">
+<div class="section is-medium">
     <div class="container">
-        <div class="section has-text-centered flex-center">
+        <div class="section has-text-centered-desktop flex-center">
             <div class="section-marker">Folio</div>
         </div>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae itaque fuga, tempore alias totam amet
-            beatae ullam molestias exercitationem illum nemo asperiores, nobis consequatur, fugiat voluptatibus nihil! Assumenda
-            sequi perspiciatis officia quidem commodi ea corrupti, excepturi vel libero porro repellendus dolores molestiae
-            maxime cupiditate saepe in non exercitationem deserunt. Quisquam autem, consequuntur suscipit facilis nam ab
-            fugiat exercitationem magni.</p>
+        <div class="columns">
+            <div class="column" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae itaque fuga, tempore alias totam
+                    amet beatae ullam molestias exercitationem illum nemo asperiores, nobis consequatur, fugiat voluptatibus
+                    nihil! Assumenda sequi perspiciatis officia quidem commodi ea corrupti, excepturi vel libero porro repellendus
+                    dolores molestiae maxime cupiditate saepe in non exercitationem deserunt. Quisquam autem, consequuntur
+                    suscipit facilis nam ab fugiat exercitationem magni.</p>
+            </div>
+            <div class="column" data-aos="fade-left" data-aos-duration="1000"  data-aos-delay="500">
+                <div class="columns is-fluid is-multiline">
+                    <div class="column is-6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam labore alias quasi culpa rerum saepe
+                            deserunt est. Omnis, tenetur. Repudiandae!</p>
+                    </div>
+                    <div class="column is-6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam labore alias quasi culpa rerum saepe
+                            deserunt est. Omnis, tenetur. Repudiandae!</p>
+                    </div>
+                    <div class="column is-6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam labore alias quasi culpa rerum saepe
+                            deserunt est. Omnis, tenetur. Repudiandae!</p>
+                    </div>
+                    <div class="column is-6">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam labore alias quasi culpa rerum saepe
+                            deserunt est. Omnis, tenetur. Repudiandae!</p>
+                    </div>
+                </div>
 
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit recusandae itaque fuga, tempore alias totam
+                    amet beatae ullam molestias exercitationem illum nemo asperiores, nobis consequatur, fugiat voluptatibus
+                    nihil! Assumenda sequi perspiciatis officia quidem commodi ea corrupti, excepturi vel libero porro repellendus
+                    dolores molestiae maxime cupiditate saepe in non exercitationem deserunt. Quisquam autem, consequuntur
+                    suscipit facilis nam ab fugiat exercitationem magni.</p>
+            </div>
+
+        </div>
     </div>
-    </div>
+</div>

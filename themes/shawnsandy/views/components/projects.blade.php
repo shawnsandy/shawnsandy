351388b15962c3b30f8d660cc8816803f48943e3
Shawn Sandy <shawnsandy04@gmail.com>
2017-11-21 05:40:23 -0500

    Layout : portofolio displays / thumbnails

--- themes/shawnsandy/views/components/projects.blade.php
+++ themes/shawnsandy/views/components/projects.blade.php
@@ -3,45 +3,38 @@
         <div class="section has-text-centered-desktop flex-center">
             <div class="section-marker">Folio</div>
         </div>
+
+        <div class="columns is-multiline" data-aos="fade-up">
+        <div class="column is-6">
+                <img src="{{ Imgfly::img('jarvis.jpeg?w=680&h=540&fit=crop-top') }}" alt="">
+            <div class="content"><p class="subtitle is-3">Jarvis</p></div>
+        </div>
+        <div class="column is-6">
+                <img src="{{ Imgfly::img('hernando-mission.png?w=680&h=540&fit=crop-top') }}" alt="">
+            <div class="content"><p class="subtitle is-3">Hernando Mission</p></div>
+        </div>
+
+        </div>
+
+
+
         <div class="columns">
-            {{--  <div class="column" data-aos="fade-right" data-aos-duration="1000" data-aos-delay="500">
-
-            <div style="border-radius: 5px; overflow=hidden">
-                <div class="content">
-                <p class="subtitle is-2">Sample Image</p>
-                <img src="{{ Imgfly::imgFly('amotivated-me.jpeg?w=670&h=540&fit=crop-top') }}" alt="">
-                </div>
-
-            </div>
-
-            </div>  --}}
-
-            <div class="column" data-aos="fade-left" data-aos-duration="1000"  data-aos-delay="500">
+            <div class="column" data-aos="fade-up" data-aos-duration="1000"  data-aos-delay="500">
                 <div class="columns is-fluid is-multiline">
+
                     <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
-                        <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+                        <img src="{{ Imgfly::img('mariosargeant.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+                    <div class="content"><p class="subtitle is-3">Mariosargeant.com</p></div>
                     </div>
                     <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
-                        <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+                        <img src="{{ Imgfly::img('interns.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+                    <div class="content"><p class="subtitle is-3">TampabayInterns</p></div>
                     </div>
                     <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
                         <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+                    <div class="content"><p class="subtitle is-3">A Motivated Me</p></div>
                     </div>
-                    <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
-                        <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
-                    </div>
-                    <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
-                        <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
-                    </div>
-                    <div class="column is-4">
-                    <p class="subtitle is-2">Sample Image</p>
-                        <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
-                    </div>
+
                 </div>
 
             </div>

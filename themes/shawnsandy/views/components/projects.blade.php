4c7b011eae7b929d582fdc3272c2015ef012f6de
Shawn Sandy <shawnsandy04@gmail.com>
2017-12-02 21:20:28 -0500

    layout : reorganised images, add border 5px border to images

--- themes/shawnsandy/views/components/projects.blade.php
+++ themes/shawnsandy/views/components/projects.blade.php
@@ -6,25 +6,25 @@
 
         <div class="columns is-multiline" data-aos="fade-up">
         <div class="column is-6">
-                <img src="{{ Imgfly::img('jarvis.jpeg?w=680&h=540&fit=crop-top') }}" alt="">
-            <div class="content"><p class="subtitle is-3">Jarvis</p></div>
+                <img src="{{ Imgfly::img('interns.jpeg?w=680&h=540&fit=crop-top') }}" alt="">
+            <div class="content"><p class="subtitle is-3">TampaBay Intern</p></div>
         </div>
         <div class="column is-6">
-                <img src="{{ Imgfly::img('hernando-mission.png?w=680&h=540&fit=crop-top') }}" alt="">
-            <div class="content"><p class="subtitle is-3">Hernando Mission</p></div>
+                <img src="{{ Imgfly::img('ctsfla.jpeg?w=680&h=540&fit=crop-top') }}" alt="">
+            <div class="content"><p class="subtitle is-3">CTSFLA</p></div>
         </div>
 
         </div>
 
         <div class="columns is-fluid is-multiline" data-aos="fade-up" data-aos-duration="1000">
+            <div class="column is-4">
+                <img src="{{ Imgfly::img('jarvis.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
+            <div class="content"><p class="subtitle is-3">TampabayInterns</p></div>
+            </div>
 
             <div class="column is-4">
                 <img src="{{ Imgfly::img('mariosargeant.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
             <div class="content"><p class="subtitle is-3">Mariosargeant.com</p></div>
-            </div>
-            <div class="column is-4">
-                <img src="{{ Imgfly::img('interns.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
-            <div class="content"><p class="subtitle is-3">TampabayInterns</p></div>
             </div>
             <div class="column is-4">
                 <img src="{{ Imgfly::img('amotivated-me.jpeg?w=448&h=340&fit=crop-top') }}" alt="">
